ea46bd43794f1a7a635bfd378b3174241c7b5f93
Bartosz Pankowski <bartosz.pankowski@codilime.com>
2018-09-21 16:26:27 +0200

    Deleted unwanted comments, packed into function code wich is checking current sprint. Added to gitignore file venv env files. Changed requirements file.

--- jira-doc.py
+++ jira-doc.py
@@ -2,9 +2,31 @@ from __future__ import print_function
 from collections import Counter
 from jira import JIRA
 from datetime import datetime, timedelta
-import json
-import ast
 import re
+
+def get_current_sprint():
+    check_sprint = jira.search_issues('project=JD and status="In progress"')
+    print (jira.search_issues('project=JD and status="Done" and Sprint="13" ORDER BY status DESC, updated DESC'))
+    print (jira.search_issues('project=JD and status="Done" and Sprint="Sprint 13" ORDER BY status DESC, updated DESC'))
+    sprint = ''
+    status = ''
+    empty_list = []
+    for x in check_sprint:
+        if x.fields.customfield_10010 is not empty_list:
+            if status is '':
+                length = len(x.fields.customfield_10010)
+                for y in range(0,length):
+                    if x.fields.customfield_10010[y] != []:
+                        check2 = x.fields.customfield_10010[y]
+                        check3 = '['+check2.split('[')[1] 
+                        result = re.search('state=(.*),', check3)
+                        status = result.group(1).split(',')[0]
+
+                        if status == 'ACTIVE':
+                            if sprint is '':
+                                sprint_str = (check3.split('name=Sprint ')[1])
+                                sprint = sprint_str.split(',')[0]
+                                return sprint
 
 def get_list(sprint):
     query_string = ('project=JD and status="Done" and Sprint="' + sprint +'"')
@@ -29,9 +51,7 @@ def get_list(sprint):
         update_date = x.raw['fields']['updated']
         update_date = update_date.split('.', 1)[0]
         then = datetime.strptime(update_date, "%Y-%m-%dT%H:%M:%S")
-        # print(then > datetime.now() - timedelta(days = 7))
-        # print(then)
-        # print(datetime.now() - timedelta(days = 7))
+
         if then > datetime.now() - timedelta(days = 7):
             if x.raw['fields']['assignee']is None:
                 output = (x.raw['fields']['summary'])
@@ -47,10 +67,7 @@ def get_list(sprint):
     print ('In progress')
     file.write('In progress \n')
     for x in inprogress:
-        # update_date = x.raw['fields']['updated']
-        # update_date = update_date.split('.', 1)[0]
-        # then = datetime.strptime(update_date, "%Y-%m-%dT%H:%M:%S")
-        # if then > datetime.now() - timedelta(days = 7):
+
             if x.raw['fields']['assignee']is None:
                 if len(x.fields.customfield_10010) > 1:
                     opened = 'This task was included in '+ str(len(x.fields.customfield_10010)) + ' sprints.'
@@ -73,10 +90,7 @@ def get_list(sprint):
     print ('To do')
     file.write('To do \n')
     for x in todo:
-        # update_date = x.raw['fields']['updated']
-        # update_date = update_date.split('.', 1)[0]
-        # then = datetime.strptime(update_date, "%Y-%m-%dT%H:%M:%S")
-        # if then > datetime.now() - timedelta(days = 7):
+
             if x.raw['fields']['assignee']is None:
                 if len(x.fields.customfield_10010) > 1:
                     opened = 'This task was included in '+ str(len(x.fields.customfield_10010)) + ' sprints.'
@@ -106,32 +120,7 @@ for username,password in creds:
             'server': server
             }
 jira = JIRA(options, basic_auth = (username, password))
-check_sprint = jira.search_issues('project=JD and status="In progress"')
-sprint = ''
-status = ''
-empty_list = []
-# print (check_sprint)
-for x in check_sprint:
-    # print(x.fields.customfield_10010[0])
-    # print (x.fields.customfield_10010[0] is not empty_list)
-    if x.fields.customfield_10010 is not empty_list:        
-        if status is '':
-            length = len(x.fields.customfield_10010)
-            for y in range(0,length):
-                if x.fields.customfield_10010[y] != []:
-                    # print (x.fields.customfield_10010)
-                    check2 = x.fields.customfield_10010[y]
-                    check3 = '['+check2.split('[')[1] 
-                    result = re.search('state=(.*),', check3)
-                    status = result.group(1).split(',')[0]
 
-                    if status == 'ACTIVE':
-                        if sprint is '':
-                            sprint_str = (check3.split('name=Sprint ')[1])
-                            sprint = sprint_str.split(',')[0]
+get_list(get_current_sprint())
 
-                            get_list(sprint)
-                            print ('Job done brah.')
-
-                            exit()
-                           
+exit('Job done brah.')
